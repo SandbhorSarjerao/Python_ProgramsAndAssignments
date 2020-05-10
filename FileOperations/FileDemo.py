e4de51a81c20c4f92374341435c5e3dd4c4d84c1
sandbhorsarjerao <sandbhorsarjerao@gmail.com>
2020-05-10 12:06:01 +0530

    Updated File Program - FileDemo.py

--- FileOperations/FileDemo.py
+++ FileOperations/FileDemo.py
@@ -1,12 +1,14 @@
 f = open('MyData.txt', 'r')
+f1 = open('CopyMyData.txt', 'w')
+print('MyData.txt File Data ==> ')
 for data in f:
     print(data, end='')
 
-f1 = open('CopyMyData.txt', 'w')
-for data in f:
     f1.write(data)
+f.close()
 f1.close()
 
+print('CopyMyData.txt File Data ==> ')
 f1 = open('CopyMyData.txt', 'r')
 for data in f1:
     print(data, end='')
