76ce4e834ab089309eb7fc2247b60280f22a4dbb
sandbhorsarjerao <sandbhorsarjerao@gmail.com>
2020-05-13 19:30:30 +0530

    added new programs

--- Operators/Conditional-TernaryOperators.py
+++ Operators/Conditional-TernaryOperators.py
@@ -1,8 +1,7 @@
 '''
 Conditional / Ternary
-'''
-
 x = b if a else c       # x=(a)?b:c
+'''
 
 a = 3
 b = 5
@@ -19,3 +18,16 @@ b = 3
 c = 7
 minval = a if (a < b and a < c) else b if b < c else c
 print('Minimum Value: ', minval)
+
+min1 = a if a < b < c else b if b < a < c else c
+print('Minimum Value: ', min1)
+
+a = 5
+b = 35
+c = 30
+min2 = a if a < b < c else b if b < a < c else c    # Wrong Answer, Not working
+print('Minimum Value: ', min2)
+
+min3 = a if a < b < c else b if b < c else c        # Wrong Answer, Not working
+print('Minimum Value: ', min3)
+
--- Operators/RelationalOperators.py
+++ Operators/RelationalOperators.py
@@ -28,7 +28,7 @@ print(chr(97))      # a
 print(chr(98))      # b
 print('abc' > '10')     # True
 
-print('abc' > 10)  # TupeError => Not supported
+print('abc' > 10)  # TypeError => Not supported
 
 print("10 < 20 < 30 : ", 10 < 20 < 30)        # True
 print("10 < 20 < 30 > 50 : ", 10 < 20 < 30 > 50)        # False
