5a2d5e95a74239ec53850f3228dd458af7b870bd
Sarjerao Sandbhor <40741655+SandbhorSarjerao@users.noreply.github.com>
2022-02-23 17:04:07 +0530

    Create Compare2Strings.py

---
+++ String/Compare2Strings.py
@@ -0,0 +1,16 @@
+str1 = "Testing"
+str2 = "testingtesting"
+
+if(str1==str2):
+  print("Same Strings")
+else:
+  print("Not same Strings")
+  
+
+str1 = "Testing"
+str2 = "    Testing     " 
+
+if(str1.strip()==str2.strip()):
+  print("Same Strings")
+else:
+  print("Not same Strings")
