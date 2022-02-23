14f14daaf7a3d977a260fa12b1e7c6226e1de454
Sarjerao Sandbhor <40741655+SandbhorSarjerao@users.noreply.github.com>
2022-02-23 17:20:24 +0530

    Update StringReverse.py

--- String/StringReverse.py
+++ String/StringReverse.py
@@ -7,3 +7,10 @@ for l in s1:
     new_str = l + new_str
 
 print(new_str)
+
+length = len(s1)
+print("String Length is " + str(length))
+
+for i in range((length-length), -1, -1):
+    new_str = new_str + s1[i]
+print(new_str) 
