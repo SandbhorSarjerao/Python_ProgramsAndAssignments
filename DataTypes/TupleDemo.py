3bcc683f18ca9cc30b047845296363d8e77e2ea2
Sarjerao Sandbhor <40741655+SandbhorSarjerao@users.noreply.github.com>
2022-02-24 17:27:52 +0530

    Update TupleDemo.py

--- DataTypes/TupleDemo.py
+++ DataTypes/TupleDemo.py
@@ -8,3 +8,19 @@ Tuple ==>
 6) Heterogeneous Elements Allowed
 7) Size Can NOT chage
 '''
+
+tuple1 = (45, 'Automation', 'Testing', True, 12.05, 45, 45)
+tuple2 = (45, 99)
+
+print(tuple1.count(45))                                         # 3
+
+print(tuple1.index('Testing'))                                  # 3
+
+tuple3 = tuple1 + tuple2              # Merge 2 tuples
+print(tuple3) 
+
+for i in tuple3:
+  print(i)
+  
+for i in range(0, len(tuple3)):
+  print(tuple3[i])  
